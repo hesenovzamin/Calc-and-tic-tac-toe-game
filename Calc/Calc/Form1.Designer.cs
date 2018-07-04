a4d23ac868635a5e9beea13407f372a9d61d5ff0
hesenovzamin <hesenovzamin@gmail.com>
2018-07-05 00:39:08 +0400

    calc

---
+++ Calc/Calc/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Hesabla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(373, 35);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Hesabla
+            // 
+            this.Hesabla.Location = new System.Drawing.Point(729, 424);
+            this.Hesabla.Name = "Hesabla";
+            this.Hesabla.Size = new System.Drawing.Size(134, 50);
+            this.Hesabla.TabIndex = 1;
+            this.Hesabla.Text = "Hesabla";
+            this.Hesabla.UseVisualStyleBackColor = true;
+            this.Hesabla.Click += new System.EventHandler(this.Hesabla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(623, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 556);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Hesabla);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Hesabla;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Calc/Calc/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Form1 : Form
+    {
+        public string eded1="";
+        public string eded2 = "";
+        public string emeliyyat;
+        public Form1()
+        {
+            InitializeComponent();
+            Create();
+            Emeliyyatlar();
+        }
+
+        static int count = 0;
+        private void eded(object sender, EventArgs e)
+        {
+       
+            if (1>=count)
+            {
+                if (count==0)
+                {
+                    Button btn = sender as Button;
+                    textBox1.Text += btn.Text;
+                    eded1 += btn.Text;
+                }
+                else
+                {
+                    Button btn = sender as Button;
+                    textBox1.Text += btn.Text;
+                    eded2 += btn.Text;
+                }
+            }
+
+        }
+        private void emelliyat(object sender, EventArgs e)
+        {
+            count++;
+            Button btn = sender as Button;
+            textBox1.Text += btn.Text;
+            emeliyyat = btn.Text;
+        }
+        public int Emeliyyat(int a,int b,string e)
+        {
+            if (e=="+")
+            {
+               return a + b;
+            }
+            if (e == "-")
+            {
+                return a - b;
+            }
+            if (e == "*")
+            {
+                return a * b;
+            }
+            if (e == "/")
+            {
+                return a - b;
+            }
+            return a / b;
+        }
+
+        void Emeliyyatlar()
+        {
+            string[] str = new string[] {"+","-","*","/" };
+            int Toppos = 90;
+            int Leftpos = 280;
+            for (int i = 0; i <4; i++)
+            {
+                Button btn = new Button();
+                btn.Width = 60;
+                btn.Height = 60;
+                btn.Text = str[i];
+                btn.Click += emelliyat;
+                btn.Top = Toppos;
+                btn.Left = Leftpos;
+                Controls.Add(btn);
+                Toppos += 70;
+
+            }
+        }
+        void Create()
+        {
+            int Toppos = 90;
+            int Leftpos = 60;
+            for (int i = 1; i <= 11; i++)
+            {
+                Button btn = new Button();
+                btn.Width = 60;
+                btn.Height = 60;
+                btn.Text = (i - 1).ToString();
+                btn.Top = Toppos;
+                btn.Left = Leftpos;
+                btn.Click += eded;
+                Controls.Add(btn);
+                Leftpos += 70;
+                if (i % 3 == 0)
+                {
+                    Leftpos = 60;
+                    Toppos += 70;
+                }
+            }
+        }
+
+        private void Hesabla_Click(object sender, EventArgs e)
+        {
+
+            label1.Text = Emeliyyat(Convert.ToInt32(eded1), Convert.ToInt32(eded2), emeliyyat).ToString();
+        }
+    }
+}
